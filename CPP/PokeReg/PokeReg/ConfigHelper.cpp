453ddd8e0b3f3c47d0434332007da8d7f92a24e5
ASquigglyTwist <36764747+asquigglytwist@users.noreply.github.com>
2015-07-25 21:21:45 +0530

    Changes to ConfigHelper
    Changes to ConfigHelper:
    1)  Moving GetTimeStamp to CPP;
    2)  Fixed a small issue with Logging;
    3)  Adding a new command line switch for BotMode;
    4)  Adding a place holder for DisplayUsage.

--- CPP/PokeReg/PokeReg/ConfigHelper.cpp
+++ CPP/PokeReg/PokeReg/ConfigHelper.cpp
@@ -3,6 +3,10 @@
 
 namespace PokeReg
 {
+	void ConfigHelper::DisplayUsage()
+	{
+	}
+
 	bool ConfigHelper::Init(int argc, _TCHAR* argv[])
 	{
 		if(argc < 2 || !argv)
@@ -22,7 +26,7 @@ namespace PokeReg
 			}
 			else if(!_tcsicmp(tszCmd, CLA_OUT_FILE))
 			{
-				LOGDEBUG(this, _T("Argument at %d is %s."), i, CLA_IN_FILE);
+				LOGDEBUG(this, _T("Argument at %d is %s."), i, CLA_OUT_FILE);
 				i++;
 				m_wsOutFileName = argv[i];
 				m_bEnableFileLogging = true;
@@ -40,7 +44,48 @@ namespace PokeReg
 				m_bEnableDebugView = true;
 				LOGINFO(_T("Debug Output Enabled."));
 			}
+			else if(!_tcsicmp(tszCmd, CLA_MODE_BOT))
+			{
+				LOGDEBUG(this, _T("Argument at %d is %s."), i, CLA_MODE_BOT);
+				m_bBotMode = true;
+				LOGINFO(_T("Bot Mode Enabled."));
+			}
+			else
+			{
+				LOGINFO(_T("Invalid command line parameter: %s."), argv[i]);
+				DisplayUsage();
+			}
+		}
+		if(m_bEnableFileLogging)
+		{
+			if(m_wsOutFileName.empty())
+			{
+				LOGDEBUG(this, _T("No Output FileName provided; Falling back to default: %s."), OUTPUT_FILE_NAME_DEF);
+				m_wsOutFileName = OUTPUT_FILE_NAME_DEF;
+			}
 		}
 		return true;
+	}
+
+	std::wstring ConfigHelper::GetTimeStamp()
+	{
+		time_t t = time(0);   // get time now
+		struct tm now;
+		if(_localtime64_s(&now, &t))
+		{
+			LOGERROR(_T("Unable to get time stamp."));
+			return _T("");
+		}
+		std::wstringstream wssNow;
+		wssNow << L"["
+			<< (now.tm_year + 1900) << '/' 
+				<< (now.tm_mon + 1) << '/'
+				<<  now.tm_mday
+				<< L"  "
+				<< now.tm_hour << L"-"
+				<< now.tm_min << L"-"
+				<< now.tm_sec
+				<< L"]  ";
+		return wssNow.str();
 	}
 }
--- CPP/PokeReg/PokeReg/ConfigHelper.h
+++ CPP/PokeReg/PokeReg/ConfigHelper.h
@@ -11,59 +11,35 @@
 #define CLA_OUT_FILE _T("--outfile")
 #define CLA_ENABLE_DEBUG_OUT _T("--enabledebugout")
 #define CLA_ENABLE_DEBUG_VIEW _T("--enabledebugview")
+#define CLA_MODE_BOT _T("--botmode")
 
 namespace PokeReg
 {
 	class ConfigHelper
 	{
-		bool m_bEnableDebug, m_bEnableDebugView, m_bEnableFileLogging;
+		bool m_bEnableDebug, m_bEnableDebugView, m_bEnableFileLogging, m_bBotMode;
 		std::wstring m_wsInFileName, m_wsOutFileName;
 		bool Init(int argc, _TCHAR* argv[]);
+		void DisplayUsage();
 	public:
 #ifdef _DEBUG
-		ConfigHelper::ConfigHelper(int argc, _TCHAR* argv[]): m_bEnableDebug(true), m_bEnableDebugView(true), m_bEnableFileLogging(true),
-			m_wsInFileName(INPUT_FILE_NAME_DEF)
+		ConfigHelper::ConfigHelper(int argc, _TCHAR* argv[]) : m_bEnableDebug(true), m_bEnableDebugView(true), m_bEnableFileLogging(true),
+			m_bBotMode(false), m_wsInFileName(INPUT_FILE_NAME_DEF)
 #else
-		ConfigHelper::ConfigHelper(int argc, _TCHAR* argv[]): m_bEnableDebug(false), m_bEnableDebugView(false), m_bEnableFileLogging(false),
-			m_wsInFileName(INPUT_FILE_NAME_DEF)
+		ConfigHelper::ConfigHelper(int argc, _TCHAR* argv[]) : m_bEnableDebug(false), m_bEnableDebugView(false), m_bEnableFileLogging(false),
+			m_bBotMode(false), m_wsInFileName(INPUT_FILE_NAME_DEF)
 #endif
 		{
 			m_wsOutFileName.clear();
 			Init(argc, argv);
-#ifdef _DEBUG
-			if(m_bEnableFileLogging && m_wsOutFileName.empty())
-			{
-				LOGINFO(_T("No Output FileName provided; Falling back to default: %s."), OUTPUT_FILE_NAME_DEF);
-				m_wsOutFileName = OUTPUT_FILE_NAME_DEF;
-			}
-#endif
 		}
 		ConfigHelper::~ConfigHelper(void) {}
 
-		inline bool GetDebugEnabled() { return m_bEnableDebug; }
-		inline bool GetDebugViewEnabled() { return m_bEnableDebugView; }
+		inline bool IsDebugEnabled() { return m_bEnableDebug; }
+		inline bool IsDebugViewEnabled() { return m_bEnableDebugView; }
+		inline bool IsInBotMode() { return m_bBotMode; }
 		inline const std::wstring GetInputFileName() { return m_wsInFileName; }
 
-		static std::wstring GetTimeStamp()
-		{
-			time_t t = time(0);   // get time now
-			struct tm now;
-			if(_localtime64_s(&now, &t))
-			{
-				LOGERROR(_T("Unable to get time stamp."));
-				return _T("");
-			}
-			std::wstringstream wssNow;
-			wssNow << L"["
-				<< (now.tm_year + 1900) << '/' 
-					<< (now.tm_mon + 1) << '/'
-					<<  now.tm_mday
-					<< L"  "
-					<< now.tm_hour << L"-"
-					<< now.tm_min << L"-"
-					<< now.tm_sec
-					<< L"]  ";
-			return wssNow.str();
-		}
+		static std::wstring GetTimeStamp();
 	};
 }
