bf036544bb5c0ddd59a8bba59b4e20f8aadc98c5
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2015-08-04 16:20:02 +0530

    Adding Tyle - a tool for log tracking.  Stands for - Tail Your Logs Efficiently.

---
+++ CS/Tyle/Tyle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tyle
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+
+    public static class AppMetaData
+    {
+        public const string ApplicationName = "Tyle";
+        public const string Description = "A Windows Tail Utility...";
+        public const string CompanyName = "L";
+        public const string CopyRight = "Copyright © " + AppMetaData.CompanyName + ", 2017; All rights reserved.";
+        public const string TradeMark = AppMetaData.ApplicationName + ", " + AppMetaData.CompanyName;
+    }
+}
