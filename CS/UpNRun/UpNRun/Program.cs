2450c24747eb6298884dc8b410163f3ac4bdef90
ASquigglyTwist <36764747+asquigglytwist@users.noreply.github.com>
2015-08-05 20:39:39 +0530

    Adding UpNRun
    Adding a new tool - UpNRun - Tool that Crawls a site and analyzes it;
    Basically a SiteMonitor.

---
+++ CS/UpNRun/UpNRun/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpNRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crawler c = new Crawler("http://msnatyalaya.com/");
+            c.BeginCrawl();
+        }
+    }
+}
---
+++ CS/UpNRun/UpNRun/ValidationHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HAP = HtmlAgilityPack;
+
+namespace UpNRun
+{
+    internal static class ValidationHelper
+    {
+        internal static bool IsValidAnchor(HAP.HtmlNode ndAnchor)
+        {
+            if(ndAnchor.Attributes.Contains("href"))
+            {
+                bool bValid;
+                IsValidUri(out bValid, ndAnchor.Attributes["href"].Value);
+                return bValid;
+            }
+            return false;
+        }
+
+        internal static Uri IsValidUri(out bool isValid, string url, UriKind kind = UriKind.RelativeOrAbsolute)
+        {
+            isValid = false;
+            if(string.IsNullOrEmpty(url))
+                return null;
+            Uri temp;
+            isValid = Uri.TryCreate(url, kind, out temp);
+            return temp;
+        }
+    }
+}
