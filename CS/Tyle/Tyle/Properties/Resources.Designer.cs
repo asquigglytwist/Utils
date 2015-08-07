5d073a3531d41a04d7424f38048aef366d166037
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2015-08-08 00:00:13 +0530

    Adding new Icons.

--- CS/Tyle/Tyle/Properties/Resources.Designer.cs
+++ CS/Tyle/Tyle/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Tyle.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Tyle {
+            get {
+                object obj = ResourceManager.GetObject("Tyle", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
