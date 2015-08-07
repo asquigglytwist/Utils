98f7aec6f4ccd2662f2dea77e0f745dbd562ee3d
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2015-08-07 19:30:31 +0530

    Isolating the file tailing functionality into a separate class - TailedStream.

---
+++ CS/Tyle/Tyle/UI/dlgFind.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Forms;
+
+namespace Tyle.UI
+{
+    public partial class dlgFind : Form
+    {
+        public static dlgFind FindDialog;
+
+        static dlgFind()
+        {
+            FindDialog = new dlgFind();
+        }
+
+        protected dlgFind()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+        public string SearchText
+        {
+            get
+            {
+                return tbxSearchText.Text;
+            }
+        }
+
+        public bool WrapSearch
+        {
+            get
+            {
+                return chkWrapSearch.Checked;
+            }
+        }
+        #endregion
+
+        #region EventHandlers
+        private void tbxSearchText_TextChanged(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbxSearchText.Text))
+            {
+                btnFind.Enabled = false;
+            }
+            else
+            {
+                btnFind.Enabled = true;
+            }
+        }
+        #endregion
+    }
+}
---
+++ CS/Tyle/Tyle/UI/frmMain.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Tyle.UI
+{
+    public partial class frmMain : Form
+    {
+        private Dictionary<string, frmTailViewer> mapOpenFiles;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            ResetAppState();
+        }
+
+        private void ResetAppState()
+        {
+            mapOpenFiles?.Clear();
+            mapOpenFiles = new Dictionary<string, frmTailViewer>();
+        }
+
+        private void OpenFilesForTailing(string[] fileNames)
+        {
+            foreach(string file in fileNames)
+            {
+                var temp= file.ToLower();
+                if(mapOpenFiles.ContainsKey(temp))
+                {
+                    mapOpenFiles[temp]?.ActivateAndMaximize();
+                }
+                else
+                {
+                    var tailForm = new frmTailViewer(this, file);
+                    mapOpenFiles[temp] = tailForm;
+                    tailForm.InitTailing();
+                    var newPage = new TabPage(tailForm.Text);
+                    newPage.ToolTipText = file;
+                    tbcMDIChildren.TabPages.Add(newPage);
+                    tbcMDIChildren.SelectedTab = newPage;
+                    tbcMDIChildren.Show();
+                }
+            }
+        }
+
+        public void NotifyStoppedTailing(string fileName, string title)
+        {
+            mapOpenFiles.Remove(fileName.ToLower());
+            foreach(TabPage page in tbcMDIChildren.TabPages)
+            {
+                if(page.Text.Equals(title))
+                {
+                    tbcMDIChildren.TabPages.Remove(page);
+                    break;
+                }
+            }
+            if(tbcMDIChildren.TabPages.Count < 1)
+            {
+                tbcMDIChildren.Hide();
+            }
+        }
+
+        #region MenuEventHandlers
+        private void mnuFOpen_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == dlgOpenFile.ShowDialog())
+            {
+                OpenFilesForTailing(dlgOpenFile.FileNames);
+            }
+        }
+
+        private void mnuFClose_Click(object sender, EventArgs e)
+        {
+            ActiveMdiChild?.Close();
+        }
+
+        private void mnuFExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuFCloseAll_Click(object sender, EventArgs e)
+        {
+            foreach (Form f in MdiChildren)
+            {
+                f.Close();
+            }
+        }
+
+        private void mnuWCascade_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void mnuWTileHorizontally_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void mnuWTileVertically_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void mnuWArrangeIcons_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+        #endregion
+
+        private void frmMain_MdiChildActivate(object sender, EventArgs e)
+        {
+            var temp = (ActiveMdiChild as frmTailViewer)?.Text;
+            foreach (TabPage page in tbcMDIChildren.TabPages)
+            {
+                if (page.Text.Equals(temp))
+                {
+                    tbcMDIChildren.SelectedTab = page;
+                    break;
+                }
+            }
+        }
+
+        private void tbcMDIChildren_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            frmTailViewer childForm;
+            if (tbcMDIChildren.SelectedTab != null && mapOpenFiles.TryGetValue(tbcMDIChildren.SelectedTab.ToolTipText.ToLower(), out childForm))
+            {
+                childForm.Activate();
+            }
+        }
+    }
+}
